051012ee44fe9790cbc362a28d6780dbe1362cc7
babayan0306 <125582118+babayan0306@users.noreply.github.com>
2023-03-04 17:19:20 +0400

    first worck

---
+++ PorjectMart/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PorjectMart
+//
+//  Created by Yurka Babayan on 03.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var finalButton: UILabel!
+    @IBOutlet weak var mySlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        sender.minimumValue = 0
+        sender.maximumValue = 100
+        myLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func enterButton(_ sender: UIButton) {
+        finalButton.text = myLabel.text
+        sender.backgroundColor = .yellow
+    }
+    
+    
+}
+
